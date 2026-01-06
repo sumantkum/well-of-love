f9c5a1f8a1d6651592d7593cbb91ac38893d8c76
Sumant Kumar pandit <sumantpandit993127@gmail.com>
2026-01-06 11:10:20 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className=' bg-amber-200 text-3xl pt-4 ' >Hello Well of Love Assigement</h1>
+    </>
+  )
+}
+
+export default App
